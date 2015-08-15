725c5edee7ed311c56a83f7b79550219c3f23c64
javapapo <javapapo@mac.com>
2015-08-15 19:33:43 +0200

    - bash script for casks setting up my env

---
+++ papocasks.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+echo "Installing apps with brew and cask version 1.0 by javapapo@mac.com date: 15/08/2015"
+
+echo "Brew  apps"
+
+brew install maven
+brew install ant
+brew install gradle
+
+echo "Cask apps"
+
+brew cask install the-unarchiver #uzip and stuff
+brew cask install keka #zip/unzip and much more
+
+#dev stuff
+brew cask install java
+brew cask install intellij-idea
+brew cask install eclipse-java
+brew cask install sourcetree # git client
+brew cask install slack #collaboration tool
+brew cask install sublime-text #text editor
+brew cask install mongodb #mongo db
+brew cask install robomongo #client for mongodb
+
+
+#Browsers
+brew cask install firefox
+brew cask install google-chrome
+
+# Cloud drives
+brew cask install dropbox
+brew cask install google-drive
+
+brew cask install thunderbird
+brew cask install libreoffice
+
+#Containers and stuff
+brew cask install dockertoolbox
+brew cask install vagrant
+brew cask install vagrant-manager #excellent GUI app for vagrant
+
+#Instant Messengers
+brew cask install adium
+
+#torrent client
+brew cask install utorrent #mtorrent
+
+#subs 
+brew cask install subtitles
+
+
+#game Section
+brew cask install battle-net #I need to play wow
+brew cask install wowmatrix #to install addons to wow!
+brew cask install steam #steam client
+
+
+#macosx specific
+brew cask install alfre #of course alfred!
+brew cask install menumeters #menu meters for mac
+brew cask install appcleaner #install/ uninstall for mac
+
+#media players
+brew cask install vlc
+brew cask install mplayerx
+
+#remote control
+brew cask install teamviewer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
