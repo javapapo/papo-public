44a819db57a958a2c350d1285c8932afcb31ec58
javapapo@mac.com <javapapo@mac.com>
2016-04-20 15:40:08 +0100

    Update bash_profile.sh

--- bash_profile.sh
+++ bash_profile.sh
@@ -1,19 +1,12 @@
-#set my main path for things so I dont have to change (work/home)
-MAIN_PATH=/Volumes/ssd
 
 #making Java8 as our default java for the system
 export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
-#export JAVA_7_HOME=$(/usr/libexec/java_home -v1.7)
 export JAVA_HOME=$JAVA_8_HOME
 
-#VAGRANT
-export VAGRANT_HOME=$MAIN_PATH/vagrant-images/vagrant_home
 
 #KUBERNETES
-KUBE_PATH=$MAIN_PATH/kubernetes/102
 export KUBERNETES_PROVIDER=vagrant
 export VAGRANT_DEFAULT_PROVIDER=virtualbox
-export KUBECTL=$KUBE_PATH/platforms/darwin/amd64/
 
 #go
 export GOPATH=~/go
@@ -24,12 +17,14 @@ export PATH=/usr/local/git/bin/:$KUBECTL:$GOROOT:$PATH
 
 
 # -------- ALIASES ----------------------------- #
+#git
+alias gitlog='git log --oneline --graph'
 
 #BREW
-alias brewupdateall='brew update;brew upgrade;brew cask update;brew cask cleanup'
+alias brewupdate='brew update;brew upgrade;brew cask update;brew cask cleanup'
+
 #JAVA
 alias java8='export JAVA_HOME=$JAVA_8_HOME'
-alias java7='export JAVA_HOME=$JAVA_7_HOME'
 
 #DOCKER
 eval "$(docker-machine env default 2>/dev/null)"
