5487342360e9fa76398ecc880056c1b8d40d361d
javapapo@mac.com <javapapo@mac.com>
2019-05-25 10:34:08 +0100

    Delete duti.sh

--- duti.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-duti -s com.jetbrains.intellij .java all;
-duti -s com.sublimetext.2 .sh  all;
-duti -s com.sublimetext.2 .md  all;
-duti -s com.sublimetext.2 .txt  all;
-duti -s com.sublimetext.2 .json  all;
-duti -s com.sublimetext.2 .xml  all;
-duti -s com.sublimetext.2 .adoc  all;
